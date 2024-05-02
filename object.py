b07430534236766a9625bd6254733d79d0d592f0
royazizbarera <royazizbarera@gmail.com>
2024-05-02 13:18:05 +0700

    Modify: object

--- object.py
+++ object.py
@@ -10,16 +10,23 @@ def object(src):
         model = YOLO('best.pt')
 
         # Menjalankan model pada video yang disediakan dengan opsi yang diatur
-        for result in model(source= src, show=True, save=True, stream=True, show_boxes=True, optimize=True, save_txt=False, verbose=False):
-            
-            # Cek apakah model mendeteksi sesuatu
+        frame_video = model(
+            source= src,
+            show=False, 
+            save=True,
+            stream=True, 
+            show_boxes=True,
+            optimize=True,
+            save_txt=False, 
+            verbose=False
+        )
+        for result in frame_video:
             if result:
                 logging.info("Fall Detected")
+                return True
             else:
                 logging.info("Normal")
-    
+                return False
     except Exception as e:
         logging.error(f"Error: {e}")
-
-if __name__ == "__main__":
-    object()
+        return None
