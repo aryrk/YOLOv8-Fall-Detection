5aa62684293901909e480dfaded7e76bb0305eea
royazizbarera <royazizbarera@gmail.com>
2024-05-02 13:58:52 +0700

    Modify: object detection and optimation

--- object_detection.py
+++ object_detection.py
@@ -1,39 +1,35 @@
-import logging
+# import logging
 from ultralytics import YOLO
 
 # Konfigurasi pengaturan logging
-logging.basicConfig(level=logging.INFO, format='%(message)s')
+# logging.basicConfig(level=logging.INFO, format='%(message)s')
+model_object = 'model/600_epoch/best.pt'
+model = YOLO(model_object)
 
 def object_detection(source):
-    model = 'model/600_epoch/best.pt'
-    try:
-        # Inisialisasi model YOLO dengan menggunakan model terbaik ('best.pt')
-        model = YOLO(model)
+    # Inisialisasi model YOLO dengan menggunakan model terbaik ('best.pt')
 
-        # Menjalankan model pada video yang disediakan dengan opsi yang diatur
-        frame_video = model(
-            source= source,
-            show=False,
-            save=False,
-            stream=True,
-            show_boxes=False,
-            optimize=True,
-            save_txt=False,
-            half=False,
-            max_det=1,
-            stream_buffer=True,
-            show_labels=False,
-            show_conf=False,
-            verbose=False
-        )
-        
-        for result in frame_video:
-            if result:
-                logging.info("Fall Detected")
-                return True
-            else:
-                logging.info("Not Fall Detected")
-                return False
-    except Exception as e:
-        logging.error(f"Error: {e}")
-        return None
+    # Menjalankan model pada video yang disediakan dengan opsi yang diatur
+    frame_video = model(
+        source= source,
+        show=False,
+        save=False,
+        stream=True,
+        show_boxes=False,
+        optimize=True,
+        save_txt=False,
+        half=False,
+        max_det=1,
+        stream_buffer=True,
+        show_labels=False,
+        show_conf=False,
+        verbose=False
+    )
+    for result in frame_video:
+        if result:
+            # logging.info("Fall Detected")
+            return True
+        else:
+            # logging.info("Not Fall Detected")
+            return False
+    return None
